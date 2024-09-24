13669db82ee114c099ac728af1c8bf53655fb504
Brandon <89342970+letsgobjones@users.noreply.github.com>
2024-09-24 13:36:14 +0900

    Initial Commit

---
+++ CineFlick/CineFlickApp.swift
@@ -0,0 +1,17 @@
+//
+//  CineFlickApp.swift
+//  CineFlick
+//
+//  Created by Brandon Jones on 9/24/24.
+//
+
+import SwiftUI
+
+@main
+struct CineFlickApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CineFlickTests/CineFlickTests.swift
@@ -0,0 +1,17 @@
+//
+//  CineFlickTests.swift
+//  CineFlickTests
+//
+//  Created by Brandon Jones on 9/24/24.
+//
+
+import Testing
+@testable import CineFlick
+
+struct CineFlickTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
