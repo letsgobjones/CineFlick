9e84e59cd55284fac25e9f0898f129f42368b761
Brandon <89342970+letsgobjones@users.noreply.github.com>
2024-09-24 14:14:13 +0900

    Created a Constant struct to manage constant values.

---
+++ CineFlick/Utilities/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  CineFlick
+//
+//  Created by Brandon Jones on 9/24/24.
+//
+
+
+struct Constants {
+  
+  struct Keys {
+    static let omdbAPIKey = OMDBAPI_API_KEY
+  }
+  
+}
